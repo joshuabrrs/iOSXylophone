54a6e41538a19bd1bf352f33d825cdb1a01863ff
joshuabrrs <jgb16@my.fsu.edu>
2018-03-29 23:42:46 -0400

    added sound on the view controller with AVFoundation

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -1,8 +1,10 @@
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController{
-    
+   
+    var xylophoneSound = NSURL(fileURLWithPath: Bundle.main.path(forResource: "note1", ofType: "wav")!)
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,7 +13,7 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
+     
         print(sender.tag)
         
     }
